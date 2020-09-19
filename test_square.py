9a4222394c0b95dca5072ed0956456387f5f69bd
maclinelewis <47381672+maclinelewis@users.noreply.github.com>
2020-09-19 14:23:19 +0530

    Update test_square.py

--- test_square.py
+++ test_square.py
@@ -1,12 +1,46 @@
 import math
 
+count = 0
+data={}
 def test_sqrt():
    num = 25
+   x = math.sqrt(num)
    assert math.sqrt(num) == 5
+   #out_file = open("myfile.json", "w")     
+   #json.dump(math.sqrt(num) == 5, out_file, indent = 6)  
 
 def testsquare():
    num = 7
-   assert 7*7 == 49
+   y = 7*7
+   assert 7*7 == 40
 
-def tesequality():
-   assert 10 == 11
+import json  
+    
+# python object(dictionary) to be dumped  
+dict1 ={  
+    "0": {  
+        "outcome": math.sqrt(25) == 5,  
+        "test_case_name": "test_sqrt",  
+        "atest_performed": "Suare",  
+        "frames_per_second": "0.0",
+        "result": [
+            "sqaure: 5"
+        ]
+    },  
+    "1": {  
+        "outcome": 7*7 == 40,  
+        "test_case_name": "testsquare",  
+        "test_performed": "square",  
+        "frames_per_second": "0.0",
+        "result": [
+            "sqaure: 49"
+        ]
+    },  
+}  
+    
+# the json file where the output must be stored  
+out_file = open("result.json", "w")  
+    
+json.dump(dict1, out_file, indent = 6)  
+    
+out_file.close() 
